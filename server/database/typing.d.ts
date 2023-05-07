d58287b0079f6dc2aacf07dd1c6dea3a137adbd5
Emiya <2914034404@qq.com>
2023-05-08 02:20:09 +0800

    chore

--- server/database/typing.d.ts
+++ server/database/typing.d.ts
@@ -1,10 +1,7 @@
 import type { DB as KyselyDatabase } from 'kysely-codegen/dist/db.d'
 
-import type { ColumnType, Selection } from 'kysely'
-import { From } from 'kysely/dist/cjs/parser/table-parser'
 import { Selectable } from 'kysely/dist/cjs/util/column-type'
 import { AnyColumn } from 'kysely/dist/cjs/util/type-utils'
-import { SelectAllQueryBuilder } from 'kysely/dist/cjs/parser/select-parser'
 
 type AllSelection<DB, TB extends keyof DB> = Selectable<{
   [C in AnyColumn<DB, TB>]: {
