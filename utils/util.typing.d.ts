784765319fca506dab57b5c1018924ef3ba61103
Emiya <2914034404@qq.com>
2023-06-28 01:28:17 +0800

    dev

--- utils/util.typing.d.ts
+++ utils/util.typing.d.ts
@@ -1,6 +1,5 @@
-export type UIProps<T extends Record<string, unknown> = unknown> = T &
-  React.HTMLAttributes<Element> & {
-    className?: string
-    width?: string | number
-    height?: string | number
-  }
+export type UIProps<T extends Record<string, unknown> = unknown> = T & {
+  className?: string
+  width?: string | number
+  height?: string | number
+}
