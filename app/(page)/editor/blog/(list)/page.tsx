1084564a2bb8b9bc7639d8640082a30784657928
Emiya <emiya@emiya.com.cn>
2023-07-10 01:57:30 +0800

    dev (#26)

--- app/(page)/editor/blog/(list)/page.tsx
+++ app/(page)/editor/blog/(list)/page.tsx
@@ -4,22 +4,29 @@ import Link from 'next/link'
 import Card from '@/components/Card'
 import { postAPI, getAPI } from '@/utils/http'
 import usePromise from '@/utils/hooks/usePromise'
+import classNames from 'classnames'
+import { Blog } from '@/server/database/typing'
 
 export default function EditorListPage() {
   const { data } = usePromise(getAPI('getEditorBlogList'))
+
+  const changeData = (id: number, data: Partial<Blog>) => {
+    postAPI('updateBlog', id, data)
+  }
 
   return (
     <div>
       {data?.map((item, index) => (
         <div key={index}>
           <Card
+            className={classNames({ 'opacity-40': !item.visibility })}
             title={
               <h3>
                 <Link href={`/editor/blog/${item.id}`}>{item.title}</Link>
               </h3>
             }
           >
-            <select defaultValue={item.visibility} onChange={(e) => postAPI('updateBlog', item.id, { visibility: Number(e.target.value) })}>
+            <select defaultValue={item.visibility} onChange={(e) => changeData(item.id, { visibility: Number(e.target.value) })}>
               <option value="1">显示</option>
               <option value="0">隐藏</option>
             </select>
