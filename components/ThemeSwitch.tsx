6c3c98cfd2e0207679873e5f4499e2334a6a658c
Emiya <emiya@emiya.com.cn>
2023-07-26 02:26:24 +0800

    dev (#30)

--- components/ThemeSwitch.tsx
+++ components/ThemeSwitch.tsx
@@ -6,6 +6,7 @@ import { IconLight, IconDark } from '@/ui/icon'
 function ThemeSwitch(props?: { width?: number }) {
   const [mounted, setMounted] = useState(false)
   const { resolvedTheme: theme, setTheme } = useTheme()
+  const { width } = props ?? {}
 
   // https://github.com/pacocoursey/next-themes#avoid-hydration-mismatch
   useEffect(() => {
@@ -14,15 +15,15 @@ function ThemeSwitch(props?: { width?: number }) {
 
   const toggleTheme = () => setTheme(theme === 'dark' ? 'light' : 'dark')
 
-  if (!mounted) return null
+  if (!mounted) return <IconLight width={width} onClick={toggleTheme}></IconLight>
 
   return (
     <>
       <meta name="theme-color" content={theme === 'dark' ? '#1c1c1c' : '#f5f5f5'}></meta>
       {theme === 'dark' ? (
-        <IconDark width={props?.width} onClick={toggleTheme}></IconDark>
+        <IconLight width={width} onClick={toggleTheme}></IconLight>
       ) : (
-        <IconLight width={props?.width} onClick={toggleTheme}></IconLight>
+        <IconDark width={width} onClick={toggleTheme}></IconDark>
       )}
     </>
   )
