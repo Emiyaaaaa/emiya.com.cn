0ebc3716bbc7cfc5e5d47a53d72587baff013d7f
lihaozheng001 <lihaozheng001@ke.com>
2024-04-10 00:41:10 +0800

    update

--- app/(page)/blog/[title]/layout.tsx
+++ app/(page)/blog/[title]/layout.tsx
@@ -1,20 +1,16 @@
 import React from 'react'
-import { notFound } from 'next/navigation'
-import Article from '@/components/Article'
-import { serverSlideAPI } from '@/server/route'
-import any from '@/utils/any'
+
 import type { Metadata } from 'next'
 import { emiyaBlog } from '@/utils/contants'
 
 interface Props {
+  children: React.ReactNode
   params: {
     title: string
   }
 }
 
 export async function generateMetadata({ params }: Props): Promise<Metadata> {
-  // read route params
-  const blog = await serverSlideAPI.getBlogByTitle(params.title)
   const title = blog?.title ?? params.title.replace(/-/g, ' ') ?? emiyaBlog
 
   return {
@@ -25,10 +21,8 @@ export async function generateMetadata({ params }: Props): Promise<Metadata> {
   }
 }
 
-function Page({ params }: Props) {
-  const data = React.use(serverSlideAPI.getBlogByTitle(params.title))
-  if (!data) notFound()
-  return <Article data={any(data)}></Article>
+function Layout({ params, children }: Props) {
+  return <div>{children}</div>
 }
 
-export default Page
+export default Layout
--- mdx-components.tsx
+++ mdx-components.tsx
@@ -4,6 +4,10 @@ import { Code } from './ui/Code'
 
 export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
+    h1: (props) => {
+      console.log(props)
+      return <h1 className="text-2xl font-bold mt-8 mb-4" {...props} />
+    },
     code: (props) => {
       return <Code code={props.children as string} language={props.className?.split('-')?.[1]} />
     },
