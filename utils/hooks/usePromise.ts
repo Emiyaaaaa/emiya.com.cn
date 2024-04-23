6040494cb72dbd34f9fc6175c6c9dcb0919531d4
lihaozheng001 <lihaozheng001@realsee.com>
2024-04-23 18:06:23 +0800

    wip

--- utils/hooks/usePromise.ts
+++ utils/hooks/usePromise.ts
@@ -1,26 +1,27 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState } from "react";
 
-const resultCache = new Map<any, any>()
+const resultCache = new Map<any, any>();
 
-export default function usePromise<T = any>(func: Promise<T> | (() => Promise<T>)) {
-  const [data, setData] = useState<T | null>(null)
-  const [loading, setLoading] = useState(true)
+export default function usePromise<T = any>(
+	func: Promise<T> | (() => Promise<T>)
+) {
+	const [data, setData] = useState<T | null>(null);
+	const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
-    setLoading(true)
-    if (resultCache.has(func)) {
-      setData(resultCache.get(func))
-      setLoading(false)
-      return
-    } else {
-      const promise = typeof func === 'function' ? func() : func
-      promise.then((res) => {
-        resultCache.set(func, res)
-        setData(res)
-        setLoading(false)
-      })
-    }
-  }, [])
+	useEffect(() => {
+		setLoading(true);
+		if (resultCache.has(func)) {
+			setData(resultCache.get(func));
+			setLoading(false);
+		} else {
+			const promise = typeof func === "function" ? func() : func;
+			promise.then((res) => {
+				resultCache.set(func, res);
+				setData(res);
+				setLoading(false);
+			});
+		}
+	}, []);
 
-  return { data, loading }
+	return { data, loading };
 }
--- utils/http.ts
+++ utils/http.ts
@@ -1,43 +1,62 @@
-import { RouteKey, ServerSideAPIInterface, RouteString, RequestHooks } from '@/server/route'
+import type {
+	RequestHooks,
+	RouteKey,
+	RouteString,
+	ServerSideAPIInterface
+} from "@/server/route";
 
-export type APIResult<T extends RouteKey> = PromiseReturnType<ServerSideAPIInterface[T]>
+export type APIResult<T extends RouteKey> = PromiseReturnType<
+	ServerSideAPIInterface[T]
+>;
 
 export async function postAPI<T extends RouteKey>(
-  api: RouteString<T>,
-  ...data: RemoveTypeFormArray<Parameters<ServerSideAPIInterface[T]>, RequestHooks>
+	api: RouteString<T>,
+	...data: RemoveTypeFormArray<
+		Parameters<ServerSideAPIInterface[T]>,
+		RequestHooks
+	>
 ): Promise<APIResult<T>> {
-  const res = await fetch(`/api/${api}`, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({ data }),
-  })
-  if (res.status !== 200) {
-    throw new Error(`postAPI code(${res.status}): ${res.statusText}`)
-  }
-  return res.json().then((data) => data.data)
+	const res = await fetch(`/api/${api}`, {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json"
+		},
+		body: JSON.stringify({ data })
+	});
+	if (res.status !== 200) {
+		throw new Error(`postAPI code(${res.status}): ${res.statusText}`);
+	}
+	return res.json().then((data) => data.data);
 }
 
 export async function getAPI<T extends RouteKey>(
-  api: RouteString<T>,
-  ...data: RemoveTypeFormArray<Parameters<ServerSideAPIInterface[T]>, RequestHooks>
+	api: RouteString<T>,
+	...data: RemoveTypeFormArray<
+		Parameters<ServerSideAPIInterface[T]>,
+		RequestHooks
+	>
 ): Promise<APIResult<T>> {
-  const params = data.length > 0 ? `?params=${data.join(',')}` : ''
-  const url = `/api/${api}${params}`
+	const params = data.length > 0 ? `?params=${data.join(",")}` : "";
+	const url = `/api/${api}${params}`;
 
-  return fetch(url, {}).then((res) => {
-    if (res.status !== 200) {
-      return Promise.reject(new Error(`GET ${url} error, code: ${res.status} ${res.statusText}`))
-    }
-    return res.json().then((data) => data.data)
-  })
+	return fetch(url, {}).then((res) => {
+		if (res.status !== 200) {
+			return Promise.reject(
+				new Error(`GET ${url} error, code: ${res.status} ${res.statusText}`)
+			);
+		}
+		return res.json().then((data) => data.data);
+	});
 }
 
-export async function get<T = any>(url: string, params?: Record<string, any>, headers?: Record<string, any>) {
-  const res = await fetch(`${url}?${new URLSearchParams(params)}`, { headers })
-  if (!res.ok) {
-    throw new Error(`get 捕获到错误：code(${res.status}) ${res.statusText}`)
-  }
-  return res.json() as Promise<T>
+export async function get<T = any>(
+	url: string,
+	params?: Record<string, any>,
+	headers?: Record<string, any>
+) {
+	const res = await fetch(`${url}?${new URLSearchParams(params)}`, { headers });
+	if (!res.ok) {
+		throw new Error(`get 捕获到错误：code(${res.status}) ${res.statusText}`);
+	}
+	return res.json() as Promise<T>;
 }
