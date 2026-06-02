7d31acfce7bf7c9bec54da7d3344711c3267f167
emiyaaaaa <emiya@emiya.com.cn>
2026-06-02 15:30:58 +0800

    feat: add Vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,39 +7,46 @@ import rehypeSlug from "rehype-slug";
 import rehypeAutolinkHeadings from "rehype-autolink-headings";
 import rehypeExternalLinks from "rehype-external-links";
 
+import vercel from "@astrojs/vercel";
+
 export default defineConfig({
-	site: "https://emiya.com.cn",
-	output: "static",
-	integrations: [mdx()],
-	fonts: [
-		{
-			provider: fontProviders.fontsource(),
-			name: "Geist Sans",
-			cssVariable: "--font-geist",
-			weights: [400, 500, 600, 700, 900],
-			styles: ["normal"]
-		}
+  site: "https://emiya.com.cn",
+  output: "static",
+  integrations: [mdx()],
+
+  fonts: [
+      {
+          provider: fontProviders.fontsource(),
+          name: "Geist Sans",
+          cssVariable: "--font-geist",
+          weights: [400, 500, 600, 700, 900],
+          styles: ["normal"]
+      }
 	],
-	vite: { plugins: [tailwindcss()] },
-	markdown: {
-		shikiConfig: { theme: "dracula" },
-		processor: unified({
-			remarkPlugins: [remarkPangu],
-			rehypePlugins: [
-				rehypeSlug,
-				[
-					rehypeAutolinkHeadings,
-					{
-						behavior: "append",
-						properties: { className: ["anchor"] },
-						content: { type: "text", value: "#" }
-					}
-				],
-				[
-					rehypeExternalLinks,
-					{ target: "_blank", rel: ["noopener", "noreferrer"] }
-				]
-			]
-		})
-	}
+
+  vite: { plugins: [tailwindcss()] },
+
+  markdown: {
+      shikiConfig: { theme: "dracula" },
+      processor: unified({
+          remarkPlugins: [remarkPangu],
+          rehypePlugins: [
+              rehypeSlug,
+              [
+                  rehypeAutolinkHeadings,
+                  {
+                      behavior: "append",
+                      properties: { className: ["anchor"] },
+                      content: { type: "text", value: "#" }
+                  }
+              ],
+              [
+                  rehypeExternalLinks,
+                  { target: "_blank", rel: ["noopener", "noreferrer"] }
+              ]
+          ]
+      })
+	},
+
+  adapter: vercel()
 });
